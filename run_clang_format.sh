c79434ac3ea2a78e4eacd3c8b6cd751e45dcbe16
Dmytro Shtrikker <shtrikker28@gmail.com>
2024-04-10 14:23:37 +0300

    update run clang-format script

--- run_clang_format.sh
+++ run_clang_format.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-find main components -iname *.h -o -iname *.c | xargs clang-format-16 -i
+DISTRO=''
+
+if [ -f "/etc/debian_version" ]; then
+  DISTRO='debian'
+fi
+
+if [ -x "$(command -v clang-format-16)" ]; then
+  find src -iname '*.h' -o -iname '*.c' | xargs clang-format-16 -i
+else
+  echo "clang-format-16 is not installed"
+  case $DISTRO in
+    debian)
+      echo "Try 'sudo apt install clang-format-16'"
+      ;;
+  esac
+fi
